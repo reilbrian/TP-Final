9a82c038bacbb03ebe172a162cb2e873315a3821
Alejo Marcantoni <alejomarcantoni10@gmail.com>
2023-04-20 15:19:41 -0300

    commits

---
+++ src/test/java/org/example/example1.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.function.BooleanSupplier;
+
+public class example1 {
+    public static Integer partido(Integer goles_equipo1_id,Integer goles_equipo2_id, Integer equipo1_id, Integer equipo2_id ) {
+        return goles_equipo1_id;
+    }
+
+    public static BooleanSupplier esMayorQue(Integer goles_equipo1_id, Integer goles_equipo2_id) {
+        return null;
+    }
+
+    public static Object igualar(Integer goles_equipo1_id, Integer goles_equipo2_id) {
+        return null;
+    }
+}
